3037ada228b87302c4be771f9ae3b606afb6b9a3
eosinophilic <jaredsaquing@gmail.com>
2016-05-11 15:05:36 -0400

    started adding health system and stage hazards

---
+++ Assets/HealthManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthManager : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public int health;
+    
+    public void damage(int damage) {
+        health -= damage;
+        if (health <= 0) {
+            health = 0;
+            //kill();
+        }
+    }
+}
