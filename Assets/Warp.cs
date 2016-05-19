cb48bb8913abdebcc6c41d12eef79e1e596b856f
Eonothem <toplel>
2016-05-19 17:26:48 -0400

    Added warping to transforms

--- Assets/Warp.cs
+++ Assets/Warp.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Warp : MonoBehaviour {
     public Vector2 warpPoint;
+	public Transform exit;
 
 	// Use this for initialization
 	void Start () {
@@ -11,11 +12,15 @@ public class Warp : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Debug.Log (exit == null);
 	}
 
     void OnTriggerEnter2D(Collider2D coll) {
         Debug.Log("Warp activated");
-        coll.gameObject.transform.position = warpPoint;
+		if (exit != null) {
+			coll.gameObject.transform.position = exit.transform.position;
+		} else {
+			coll.gameObject.transform.position = warpPoint;
+		}
     }
 }
