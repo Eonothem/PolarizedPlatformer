bd9eaa34d6e99a20a3dfc20131a0ee59e586874c
Eonothem <jimmyskripchuk@gmail.com>
2016-06-14 19:16:17 -0400

    Started working on combat

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,39 +9,55 @@ public class Player : MonoBehaviour, IDamageable {
 	bool facingRight = true;
 	Rigidbody2D rigid;
 	bool grounded = false;
-	public Transform groundCheck;
+	//public Transform groundCheck;
 	float groundRadius = 0.2f;
 	public LayerMask whatIsGround;
 	bool onPlatform = false;
 	GameObject currentPlat = null;
     public Vector2 respawn = new Vector2((float)-9.5, (float)-2.1);
+	public GameObject groundCheck;
 
     //HealthManager hm = null;
-	
+	void Awake(){
+		//QualitySettings.vSyncCount = 0;
+		//Application.targetFrameRate = 60;
+	}
+
 	// Use this for initialization
 	void Start () {
 		rigid = GetComponent<Rigidbody2D> ();
+
         //hm = gameObject.GetComponent<HealthManager>();
     }
 	
 	void Update () {
+		//Debug.Log (groundCheck.GetComponent<GroundCheck> ().getGrounded ());
 	}
 	
 	void FixedUpdate ()
 	{	
 		//Debug.Log (grounded);
 		//Checks if we hit the ground or not using bounding box magic
-		BoxCollider2D bc = transform.GetComponentInParent<BoxCollider2D> ();
-		float groundHeight = 1f;
-		grounded = Physics2D.OverlapArea (new Vector2 (transform.position.x-bc.bounds.extents.x, transform.position.y), new Vector2 (transform.position.x+bc.bounds.extents.x, transform.position.y-groundHeight), whatIsGround);
-		
+		//BoxCollider2D bc = transform.GetComponentInParent<BoxCollider2D> ();
+		//float groundHeight = 1f;
+		//grounded = Physics2D.OverlapArea (new Vector2 (transform.position.x-bc.bounds.extents.x, transform.position.y), new Vector2 (transform.position.x+bc.bounds.extents.x, transform.position.y-groundHeight), whatIsGround);
+		grounded = groundCheck.GetComponent<GroundCheck>().getGrounded();
+		//Debug.Log (grounded);
 
 		//Get Input
 		float move = Input.GetAxis ("Horizontal");
 
 		//Jump
-		if (Input.GetKeyDown (KeyCode.Space) && grounded) {
-			rigid.velocity = new Vector2(rigid.velocity.x, 10);
+		if (Input.GetKeyDown (KeyCode.UpArrow) && grounded) {
+			rigid.velocity = new Vector2(rigid.velocity.x, 20);
+		}
+
+		if (!grounded) {
+			
+			if (Input.GetKeyDown(KeyCode.DownArrow)){
+				//Debug.Log ("AAAAAA");
+				rigid.velocity = new Vector2 (rigid.velocity.x, -20);
+			}
 		}
 
 		//Move left and right and be affected by gravity
@@ -64,6 +80,7 @@ public class Player : MonoBehaviour, IDamageable {
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		//When we fall on a platform, parent our character to it so that it moves along with the platform
+		//Debug.Log("Ayy");
 		if (coll.gameObject.tag == "Platform") {
 			currentPlat = coll.gameObject;
 			onPlatform = true;
@@ -72,6 +89,7 @@ public class Player : MonoBehaviour, IDamageable {
 	}
 
 	void OnCollisionExit2D(Collision2D coll) {
+		//Debug.Log ("LMAO");
 		//When we leave the platform, unparent
 		if (coll.gameObject.tag == "Platform") {
 			currentPlat = null;
@@ -81,7 +99,7 @@ public class Player : MonoBehaviour, IDamageable {
 	}
 
     void OnTriggerEnter2D(Collider2D coll) {
-        Debug.Log("Player collided with trigger");
+        //Debug.Log("Player collided with trigger");
         if (coll.gameObject.tag == "Hazard") {
             StageHazard haz = coll.gameObject.GetComponent<StageHazard>();
             damage(haz.damage);
