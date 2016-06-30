69b94cfef6c69848d678d2097426204f403d1c7d
Eonothem <jimmyskripchuk@gmail.com>
2016-06-29 20:06:32 -0400

    DialogStuff

--- Assets/DialogHandler.cs
+++ Assets/DialogHandler.cs
@@ -5,6 +5,8 @@ public class DialogHandler : MonoBehaviour {
 	private CircleCollider2D trigger;
 	private bool inRadius;
 	private bool talking;
+	public GameObject dialogUIObject;
+	private bool crRunning = false;
 	// Use this for initialization
 	void Start () {
 		trigger = gameObject.GetComponent<CircleCollider2D> ();
@@ -13,7 +15,14 @@ public class DialogHandler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//Debug.Log (inRadius);
+		//talking = dialogUIObject.GetComponent<TextScroller> ().active;
+		if (!dialogUIObject.GetComponent<TextScroller> ().active && talking && !crRunning ) {
+			Debug.Log ("AA");
+			crRunning = true;
+			StartCoroutine (endTalk());
+
+
+		}
 		if (inRadius && !talking && Input.GetKeyDown (KeyCode.E)) {
 			//Debug.Log ("Talk!");
 			activateDialogBox();
@@ -22,8 +31,9 @@ public class DialogHandler : MonoBehaviour {
 	}
 
 	private void activateDialogBox(){
-		Debug.Log ("Talking!");
-	}
+		//Debug.Log ("Talking!");
+		dialogUIObject.GetComponent<TextScroller> ().activate();
+	}     
 
 	void OnTriggerEnter2D(Collider2D coll){
 		if (coll.tag == "Player") {
@@ -37,4 +47,18 @@ public class DialogHandler : MonoBehaviour {
 			inRadius = false;
 		}
 	}
+
+	IEnumerator endTalk(){
+
+		for (int i = 0; i < 30; i++)
+		{
+			//Debug.Log (i);
+			//textBox.text = goatText[currentlyDisplayingText].Substring(0, i);
+			yield return null;
+		}
+		crRunning = false;
+		talking = false;
+		//Debug.Log ("AfdsfA");
+	}
+
 }
--- Assets/UI/TextScroller.cs
+++ Assets/UI/TextScroller.cs
@@ -5,36 +5,75 @@ using UnityEngine.UI;
 public class TextScroller : MonoBehaviour {
 	public Text textBox;
 	//Store all your text in this string array
-	string[] goatText = new string[]{"Flintstones.","Meet the Flintstones.", "They're the modern stone age family.","From the", "town of Bedrock,","They're a page right out of history."};
+	string[] goatText = new string[]{"Flintstones","Meet the Flintstones.", "They're the modern stone age family.","From the", "town of Bedrock,","They're a page right out of history."};
 	int currentlyDisplayingText = 0;
 	private int charPerFrame = 1;
+	public bool active = false;
+	public GameObject dialogPanel;
+	AudioSource audioSource;
 
 	void Awake () {
-		StartCoroutine(AnimateText());
+		//StartCoroutine(AnimateText());
+		//active = false;
+		audioSource = gameObject.GetComponent<AudioSource>();
 	}
 
 	void Update(){
-		if (Input.GetKeyDown (KeyCode.E)){
-			SkipToNextText ();
+		dialogPanel.SetActive (active);
+		if (!active) {
+			currentlyDisplayingText = 0;
 		}
+		//Debug.Log (active);
+		//Debug.Log ("AAA");
+		//Debug.Log (currentlyDisplayingText);
+		if (Input.GetKeyDown (KeyCode.E) && active){
+			
+			if (currentlyDisplayingText < goatText.Length-1) {
+				//Debug.Log ("A");
+				SkipToNextText ();
+				currentlyDisplayingText++;
+
+
+			} else {
+				//currentlyDisplayingText = 0;
+				//SkipToNextText ();
+				//Debug.Log("AA");
+				active = false;
+				//currentlyDisplayingText = 0;
+			}
+
+		
+
+			//Debug.Log (currentlyDisplayingText == goatText.Length);
+		}
+	}//
+
+	public void activate(){
+		active = true;
+		StartCoroutine(AnimateText());
 	}
+
 	//This is a function for a button you press to skip to the next text
 	public void SkipToNextText(){
 		StopAllCoroutines();
-		currentlyDisplayingText++;
+		//currentlyDisplayingText++;
 		//If we've reached the end of the array, do anything you want. I just restart the example text
-		if (currentlyDisplayingText>=goatText.Length) {
-			currentlyDisplayingText=0;
-		}
-		StartCoroutine(AnimateText());
+		//if (currentlyDisplayingText >= goatText.Length) {
+			
+			//endOfText();
+		//} else {
+			
+			StartCoroutine (AnimateText ());
+		//}
 	}
 	//Note that the speed you want the typewriter effect to be going at is the yield waitforseconds (in my case it's 1 letter for every      0.03 seconds, replace this with a public float if you want to experiment with speed in from the editor)
 	IEnumerator AnimateText(){
 
-		for (int i = 0; i < (goatText[currentlyDisplayingText].Length+1); i=i+charPerFrame)
+		for (int i = 0; i < (goatText[currentlyDisplayingText].Length+1); i+=2)
 		{
 			textBox.text = goatText[currentlyDisplayingText].Substring(0, i);
-			yield return null;
+			audioSource.Play ();
+			yield return new WaitForSeconds(0.03f);
 		}
 		//Debug.Log ("AAA");
 		textBox.text = goatText[currentlyDisplayingText].Substring(0, goatText[currentlyDisplayingText].Length);
@@ -42,5 +81,11 @@ public class TextScroller : MonoBehaviour {
 			//Debug.Log (i);
 			//yield return null
 		//}
+	}
+
+	public void endOfText(){
+		active = false;
+		currentlyDisplayingText=0;
+		//Debug.Log (goatText [currentlyDisplayingText]);
 	}
 }
